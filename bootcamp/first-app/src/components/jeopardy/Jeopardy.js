3ec569b69d8a0685083f13c688a3f5b627ab0022
Aravindakumar V M <aravindakumar_vijayasrimohankumar@intuit.com>
2024-03-26 17:07:13 -0700

    Jeopardy game hard mode

--- bootcamp/first-app/src/components/jeopardy/Jeopardy.js
+++ bootcamp/first-app/src/components/jeopardy/Jeopardy.js
@@ -4,17 +4,22 @@ import JeopardyDisplay from "./JeopardyDisplay";
 function Jeopardy(props) {
   const [score, setScore] = useState(0);
   const [jeopardyData, setJeopardyData] = useState({});
+  const [selectedOption, setSelectedOption] = useState("LANDMARKS");
   const answerField = useRef(null);
+  const categoriesMap = { LANDMARKS: 22248, KNIGHTS: 22096, JULIUS: 21726 };
 
   //when the component mounts, get a the first question
   useEffect(() => {
     getNewQuestion();
-  }, [score]);
+  }, [score, selectedOption]);
 
   //get a new random question from the API and add it to the data object in state
   const getNewQuestion = () => {
+    
+    const categoryId = categoriesMap[selectedOption];
+
     //use fetch to make an API call and get a random Jeopardy question (returns a promise)
-    fetch(`https://jservice.xyz/api/random-clue`)
+    fetch(`https://jservice.xyz/api/clues?category=${categoryId}`)
       //on success of the fetch request, turn the response that came back into JSON
       .then((response) => response.json())
       //on success of turnig the response into JSON (data we can work with), lets add that data to state
@@ -22,8 +27,11 @@ function Jeopardy(props) {
         //put the data in the console just so we can see it
         console.log("data from the api", data);
 
+        const maxValue = data.clues.length - 1;
+        const index = Math.floor(Math.random() * (maxValue + 1));
+
         //update state with the data from the API causing the page to re-render
-        setJeopardyData(data);
+        setJeopardyData(data.clues[index]);
       })
       //handle any errors/failures with getting data from the API
       .catch((error) => {
@@ -42,14 +50,22 @@ function Jeopardy(props) {
     answerField.current.value = "";
   };
 
+  const handleDropdown = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
   //present the results to the user
   return (
-    <JeopardyDisplay
-      jeopardyData={jeopardyData}
-      score={score}
-      handleSubmit={handleSubmit}
-      answerField={answerField}
-    />
+    <div className="Jeopardy">
+      <JeopardyDisplay
+        jeopardyData={jeopardyData}
+        score={score}
+        handleSubmit={handleSubmit}
+        answerField={answerField}
+        selectedOption={selectedOption}
+        handleDropdown={handleDropdown}
+      />
+    </div>
   );
 }
 
--- bootcamp/first-app/src/components/jeopardy/JeopardyDisplay.js
+++ bootcamp/first-app/src/components/jeopardy/JeopardyDisplay.js
@@ -3,24 +3,33 @@ function JeopardyDisplay(props) {
   const answerField = props.answerField;
   const handleSubmit = props.handleSubmit;
   const score = props.score;
-
-  const category = jeopardyData.category || {};
+  const selectedOption = props.selectedOption;
+  const handleDropdown = props.handleDropdown;
 
   return (
-    <div className="Jeopardy">
+    <div className="JeopardyDisplay">
       {/* Displaying the question to help you get started */}
       <div>
         <strong>Quiz:</strong>
       </div>
       <br />
+      <div>Please select a category:</div>
+      <select id="dropdown" value={selectedOption} onChange={handleDropdown}>
+        <option value="LANDMARKS">LANDMARKS</option>
+        <option value="KNIGHTS">KNIGHTS</option>
+        <option value="JULIUS">JULIUS</option>
+      </select>
+      <br />
+      <br />
       <div>
-        <strong>Question:</strong> {jeopardyData.question}
+        <strong>Question:</strong> {jeopardyData?.question || "Dummy Question"}
       </div>
       <div>
-        <strong>Category Title:</strong> {category.title || "Dummy"}
+        <strong>Category Title:</strong>{" "}
+        {jeopardyData?.category?.title || "Dummy Title"}
       </div>
       <div>
-        <strong>Point Value:</strong> {jeopardyData.value}
+        <strong>Point Value:</strong> {jeopardyData?.value || "Dummy Value"}
       </div>
       <hr />
       <div>
